1bfe894cb04bcdbca20d0ee51469452910c521da
priestle <nigel.d.priestley@promiliad.com>
2025-08-06 16:17:03 -0600

    Fixed monomer def indexing problem. Simplifying GA Enging structure

--- src/GAEngine.java
+++ src/GAEngine.java
@@ -42,27 +42,7 @@ public class GAEngine extends Engine {
         ArrayList<Datum> chromosomes = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < engineParameters.getPopulation(); i++) {
-            int numberOfMonomers = monomerDefs.getSize();
-            int a = random.nextInt(numberOfMonomers) + 1;
-            int b = random.nextInt(numberOfMonomers) + 1;
-            int c = random.nextInt(numberOfMonomers) + 1;
-            int d = random.nextInt(numberOfMonomers) + 1;
-            int e = 0;
-            int npfrA = monomerDefs.getNPFRMonomerNumber(a);
-            int npfrB = monomerDefs.getNPFRMonomerNumber(b);
-            int npfrC = monomerDefs.getNPFRMonomerNumber(c);
-            int npfrD = monomerDefs.getNPFRMonomerNumber(d);
-            int npfrE = 0;
-            double score = 1E10;
-            try {
-                score  = scorer.score(npfrA, npfrB, npfrC, npfrD, npfrE);
-                // todo: maybe if the scorer returned the whole object not just the score?
-            }
-            catch (Exception exception) {
-                Assertions.log("WARNING : No score calculated for [" + npfrA + "," + npfrB + "," + npfrC +
-                        "," + npfrD + "," + npfrE + "]; Assigning 1e10 as score.");
-            }
-            Datum aChromosome = new Datum(npfrA, npfrB, npfrC, npfrD, npfrE, score, 0.00, "-----");
+            Datum aChromosome = randomNewChromosome(monomerDefs);
             chromosomes.add(aChromosome);
         }
 
@@ -76,96 +56,51 @@ public class GAEngine extends Engine {
         Assertions.log("_________________________________________________________________");
         Assertions.log("\n");
 
-        // Archive/Output initial population
-        ArrayList<Datum> initialSetToArchive = new ArrayList<>();  // A record of the initially generated chromosomes
-        ArrayList<Datum> completeArchiveSet  = new ArrayList<>();  // A complete record of unique chromosomes so that scores
-                                                                   // can be looked up if already calculated.
+        outputPopulation("GENERATION " + generation, chromosomes, engineParameters.getPopulation());
 
-        for (int i = 0; i < chromosomes.size(); i++) {
-            if (!initialSetToArchive.contains(chromosomes.get(i))) {
-                initialSetToArchive.add(chromosomes.get(i));
-                completeArchiveSet.add(chromosomes.get(i));
-            }
-        }
-        String archiveFileName = Config.calculationRoot + "/" + engineParameters.getArchiveFileName();
-        FileArchive(archiveFileName, initialSetToArchive);
 
-        // While end condition not met
-        while (generation < engineParameters.getGenerations()) {
-
-            // Mutate population
-            ArrayList<Datum> mutants = mutate(chromosomes);
-
-            // Crossover population
-            ArrayList<Datum> children = crossover(mutants);
-            ArrayList<Datum> appendToFileArchive = new ArrayList<>();
-
-            // Score population
-            for (int i = 0; i < children.size(); i++) {
-                Datum test = children.get(i);
-                if (scoreExists(completeArchiveSet, test.a, test.b, test.c, test.d, test.e)) {
-                    test.score = retrieveScore(completeArchiveSet, test.a, test.b, test.c, test.d, test.e);
-                } else {
-                    test.score = scorer.score(test.a, test.b, test.c, test.d, test.e);
-                    completeArchiveSet.add(test);
-                    appendToFileArchive.add(test);
-                }
-            }
-            FileArchive(archiveFileName, appendToFileArchive);
-
-            // Population substitution: either parents are replaced by children or parents and children are ranked and
-
-            switch (engineParameters.getParentFate()) {
-
-                case "KEEP": {
-                    // Parents and children are mixed, ranked, and the top ones are kept
-                    ArrayList<Datum> combinedPopulation = new ArrayList<>();
-                    for (int i = 0; i < chromosomes.size(); i++) {
-                        combinedPopulation.add(chromosomes.get(i));
-                    }
-                    for (int i = 0; i < children.size(); i++) {
-                        if (notInArray(combinedPopulation, children.get(i))) {
-                            combinedPopulation.add(children.get(i));
-                        }
-                    }
-                    combinedPopulation.sort(Comparator.comparingDouble(d -> d.score));
-                    // Now reduce population to correct size
-                    chromosomes.clear();
-                    for (int i = 0; i < engineParameters.getPopulation(); i++) {
-                        chromosomes.add(combinedPopulation.get(i));
-                    }
-                    break;
-                }
-
-                case "KILL": {
-                    // Parents are eliminated and replaced by children
-                    chromosomes.clear();
-                    for (int i = 0; i < children.size(); i++) {
-                        chromosomes.add(children.get(i));
-                    }
-                    chromosomes.sort(Comparator.comparingDouble(d -> d.score));
-                    break;
-                }
-
-                default: {
-                    Assertions.log("**" + engineParameters.getParentFate() + "**");
-                    Assertions.log("FATAL ERROR : The GA config file doesn't have a good method for PARENT_FATE.");
-                    System.exit(1);
-                }
-            }
-
-            // Output diagnostic results
-            generation++;
-            Assertions.log("GENERATION " + generation);
-            for (int i = 0; i < chromosomes.size(); i++) {
-                Assertions.log(chromosomes.get(i).toString());
-            }
-            Assertions.log("_________________________________________________________________");
-            Assertions.log("\n");
-        }
     }
 
     // Methods
+
+    // *****************************************************************************************************************
+    private Datum randomNewChromosome(MonomerDefs monomerDefs) {
+        Random random = new Random();
+        int a = random.nextInt(monomerDefs.getSize()) + 1;
+        int b = random.nextInt(monomerDefs.getSize()) + 1;
+        int c = random.nextInt(monomerDefs.getSize()) + 1;
+        int d = random.nextInt(monomerDefs.getSize()) + 1;
+        int e = 0;  // We're only going to do tetramers for now
+        int npfrA = monomerDefs.getNPFRMonomerNumber(a);
+        int npfrB = monomerDefs.getNPFRMonomerNumber(b);
+        int npfrC = monomerDefs.getNPFRMonomerNumber(c);
+        int npfrD = monomerDefs.getNPFRMonomerNumber(d);
+        int npfrE = 0;
+        double score = 1E10;
+        try {
+            score  = scorer.score(npfrA, npfrB, npfrC, npfrD, npfrE);
+            // todo: maybe if the scorer returned the whole object not just the score?
+        }
+        catch (Exception exception) {
+            Assertions.log("WARNING : No score calculated for [" + npfrA + "," + npfrB + "," + npfrC +
+                    "," + npfrD + "," + npfrE + "]; Assigning 1e10 as score.");
+        }
+        System.out.println(npfrA + " " + npfrB + " " + npfrC + " " + npfrD + " " + npfrE);
+        Datum aChromosome = new Datum(npfrA, npfrB, npfrC, npfrD, npfrE, score, 0.00, "-----");
+        return aChromosome;
+    }
+
+    // *****************************************************************************************************************
+    private void outputPopulation(String message, ArrayList<Datum> chromosomes, int numberToPrint) {
+        Assertions.log(message);
+        for (int i = 0; i < chromosomes.size(); i++) {
+            Assertions.log(chromosomes.get(i).toString());
+        }
+        Assertions.log("_________________________________________________________________");
+        Assertions.log("\n");
+    }
+
+    // *****************************************************************************************************************
     public void FileArchive(String archiveFileName, ArrayList<Datum> additions) {
 
         try (FileWriter outputF = new FileWriter(archiveFileName, true)) {
@@ -185,14 +120,17 @@ public class GAEngine extends Engine {
 
     }
 
+    // *****************************************************************************************************************
     public ArrayList<Datum> mutate(ArrayList<Datum> individuals) {
         return individuals;
     }
 
+    // *****************************************************************************************************************
     public ArrayList<Datum> crossover(ArrayList<Datum> individuals) {
         return individuals;
     }
 
+    // *****************************************************************************************************************
     public boolean scoreExists(ArrayList<Datum> chrom, int a, int b, int c, int d, int e) {
 
         boolean isInList = false;
@@ -208,6 +146,7 @@ public class GAEngine extends Engine {
         return isInList;
     }
 
+    // *****************************************************************************************************************
     public double retrieveScore(ArrayList<Datum> chrom, int a, int b, int c, int d, int e) {
 
         for (int i = 0; i < chrom.size(); i++) {
@@ -219,6 +158,7 @@ public class GAEngine extends Engine {
         return 1E10;
     }
 
+    // *****************************************************************************************************************
     public boolean notInArray(ArrayList<Datum> chrom, Datum entry) {
 
         boolean isInArray = false;
